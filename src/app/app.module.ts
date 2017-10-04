5440f0eaea4e908911560aad551ad4218e28391a
Ankita-Palekar <453ankitapalekar@gmail.com>
2017-10-04 06:55:42 +0530

    components added to the webiet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { BlogComponent } from './blog/blog.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoaderComponent } from './loader/loader.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    WorkComponent,
+    BlogComponent,
+    FooterComponent,
+    LoaderComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule
